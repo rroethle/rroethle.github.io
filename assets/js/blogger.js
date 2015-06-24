0f04b5c367c79df9a473b062a3bbf86a0879eda6
rroethle <rroethle_he@dev.bse.edu>
2015-06-24 17:08:39 -0500

    Added blogger.js and updated index.html file to show blog at end of footer.

---
+++ assets/js/blogger.js
@@ -0,0 +1,14 @@
+$(document).ready(getposts);
+
+
+
+function getblog(){
+	<div id='key' class='col-sm-12 blogpost'> 
+	<small>
+    <p class='muted' style='float:right;'>Post date will go here.</p>
+	</small>
+  <h5>Post title will go here.</h5>
+  <p>Post content will go here.</p>
+  <hr/>
+</div>
+}
